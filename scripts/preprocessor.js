f41c9998f321f6c7744f712ed909eb711b8b29d2
Andrey Popp <8mayday@gmail.com>
2014-05-19 19:48:33 +0400

    jest: scope transform for a current package

--- scripts/preprocessor.js
+++ scripts/preprocessor.js
@@ -3,7 +3,10 @@
 var ReactTools = require('react-tools');
 
 module.exports = {
-  process: function(src) {
+  process: function(src, filename) {
+    if (filename.indexOf('node_modules') > -1) {
+      return src;
+    }
     return ReactTools.transform(src, {harmony: true});
   }
 };
